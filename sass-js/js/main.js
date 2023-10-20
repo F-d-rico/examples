e3808ce14aafc0328c85d66105383eb3cd0feb37
f_d_rico <f_d_rico@hotmail.com>
2023-10-20 01:39:50 -0300

    homeready

--- sass-js/js/main.js
+++ sass-js/js/main.js
@@ -10,7 +10,24 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 //
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
+/* document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new bootstrap.Popover(popover)
-  })
+  }) */
+
+const video = document.querySelector('#videoMides')
+const videoHandler = document.querySelector('#videoHandler')
+const videoIcon = document.querySelector('#videoHandler>i')
+
+videoHandler.addEventListener("click", playPause)
+video.addEventListener("click", playPause)
+
+function playPause() { 
+  if (video.paused) {
+    video.play();
+    videoIcon.className = "bi bi-pause-circle";
+  } else {
+    video.pause(); 
+    videoIcon.className = "bi bi-play-circle";
+  }
+} 
