351df99a021cd624336da90dc93414699bf48557
f_d_rico <f_d_rico@hotmail.com>
2023-10-26 20:53:23 -0300

    sinactionynoti

--- sass-js/js/main.js
+++ sass-js/js/main.js
@@ -15,6 +15,25 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
     new bootstrap.Popover(popover)
   }) */
 
+  (() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
 const video = document.querySelector('.video');
 const videoHandler = document.querySelector('.videoHandler');
 const iconVideoPlay = document.querySelector('.iconVideoPlay');
